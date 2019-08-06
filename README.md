9c4422be0180f257fa9d7e8ed8cf6ebafc2c0b49
babybreath <jai2012@163.com>
2019-08-06 17:10:46 +0800

    feat: 支持自定义前缀

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # gulp-html-path
+
 rewrite path as absolute or relative
+
 路径处理为绝对路径或相对路径
 
 # Install
@@ -10,29 +12,98 @@ $ npm install --save-dev gulp-html-path
 # Usage
 
 ```js
-var gulp = require('gulp');
-var gulpHtmlPath = require('gulp-html-path');
+const gulp = require('gulp');
+const gulpHtmlPath = require('gulp-html-path');
 
-var srcHtml = '**/*.html';
-var buildPath = 'dist';
+const sourcePath = 'source';
+const src = ['source/**/*.html', 'source/**/*.js', 'source/**/*.css'];
+const buildPath = 'dist';
+
+gulp.task('replaceUrl', done => {
+  gulp
+    .src(src)
+    .pipe(gulpHtmlPath({ base: sourcePath, mode: 'absolute' }))
+    .pipe(gulp.dest(buildPath))
+    .on('end', () => {
+      done();
+    });
+});
+```
+
+## gulp v3
+```js
+const gulp = require('gulp');
+const gulpHtmlPath = require('gulp-html-path');
+
+const sourcePath = 'source';
+const src = ['source/**/*.html', 'source/**/*.js', 'source/**/*.css'];
+const buildPath = 'dist';
 
 gulp.task('replaceUrl',function(){
-  return gulp.src(srcHtml)
-    .pipe(gulpHtmlPath({base: './', mode: 'relative'}))
+  return gulp.src(src)
+    .pipe(gulpHtmlPath({base: sourcePath, mode: 'absolute'}))
     .pipe(gulp.dest(buildPath))
 });
 ```
 
 # API
-gulpHtmlPath(options)
-## options
-- `base`: base path of project (default: process.cwd())
-- `mode`: build path mode ['absolute'|'relative'] (default: 'absolute')
-- `isLog`: show detail logs (default: false)
+## gulpHtmlPath(options)
 
-# API
-gulpHtmlPath(options)
-## options
-- `base`: 项目根目录 (默认: process.cwd())
-- `mode`: 生成路径模式 ['absolute'|'relative'] (默认: 'absolute')
-- `isLog`: 是否显示更多详细日志 (默认: false)
+### options.base
+Type: `string`
+
+Default: `./`
+
+base path of frontend source code
+
+### options.mode
+Type: `string`
+
+Default: `absolute`
+
+build path mode ['absolute'|'relative']
+
+### options.prefix
+Type: `string`
+
+Default: ``
+
+add the prefix string to each replacement
+
+### options.isLog
+Type: `boolean`
+
+Default: `false`
+
+show detail logs
+
+## API
+## gulpHtmlPath(options)
+
+### options.base
+Type: `string`
+
+Default: `./`
+
+前端源码目录
+
+### options.mode
+Type: `string`
+
+Default: `absolute`
+
+生成路径模式 ['absolute'|'relative']
+
+### options.prefix
+Type: `string`
+
+Default: ``
+
+添加前缀
+
+### options.isLog
+Type: `boolean`
+
+Default: `false`
+
+显示更多详细日志
--- index.js
+++ index.js
@@ -1,102 +1,108 @@
 'use strict';
 
-var path = require('path');
-var Transform = require('readable-stream/transform');
-var rs = require('replacestream');
-var istextorbinary = require('istextorbinary');
+const path = require('path');
+const Transform = require('readable-stream/transform');
+const rs = require('replacestream');
+const istextorbinary = require('istextorbinary');
 
-var PLUGINNAME = 'gulp-path';
-var REG_HTML = /([\s]href=|[\s]src=)['"][a-zA-Z_\-\d\.\/]+['"]|[\s:]url\(\s*['"]?[a-zA-Z_\-\d\.\/]+['"]?\s*\)/g;
-var REG_SUB = /([\s]href=|[\s]src=)|['"]|[\s:]url\(\s*|\s*\)/g;
-var MATCHED = 0;
-var REPLACED = 0;
+const PLUGINNAME = 'gulp-html-path';
+const REG_HTML = /([\s]href=|[\s]src=)['"][a-zA-Z_\-\d\.\/]+['"]|[\s:]url\(\s*['"]?[a-zA-Z_\-\d\.\/]+['"]?\s*\)/g;
+const REG_SUB = /([\s]href=|[\s]src=)|['"]|[\s:]url\(\s*|\s*\)/g;
 
 module.exports = function(options) {
-    var base;
-    options = options || {};
-    if(options.hasOwnProperty('base')){
-        base = path.join(process.cwd(), options.base);
-    }else{
-        base = process.cwd();
-    }
-    var buildPathMode = options.mode || 'absolute';
-    console.log('\nbase => ' + base);
-    var isLog = options.log || false;
+  options = options || {};
+  const optionsBase = options.base || './';
+  const base = path.join(process.cwd(), optionsBase, './');
+  const prefix = options.prefix || '';
+  const buildPathMode = options.mode || 'absolute';
+  console.log(
+    `\nPLUGINNAME=>${PLUGINNAME}  base=>${base}  mode=>${buildPathMode}`
+  );
+  const isLog = options.log || false;
 
-    function log(msg){
-        isLog && console.log(msg);
-    }
+  function log(msg) {
+    isLog && console.log(msg);
+  }
 
-    return new Transform({
-        objectMode: true,
-        transform: function(file, enc, callback) {
-            if (file.isNull()) {
-                return callback(null, file);
-            }
+  return new Transform({
+    objectMode: true,
+    transform: function(file, enc, callback) {
+      if (file.isNull()) {
+        return callback(null, file);
+      }
+      let MATCHED = 0;
+      let REPLACED = 0;
 
-            function doReplace() {
-                if (file.isStream()) {
-                    file.contents = file.contents.pipe(rs(search, replacement));
-                    return callback(null, file);
-                }
-
-                if (file.isBuffer()) {
-                    console.log('\nchecking file...' + file.history[0]);
-                    var fileContent = file.contents.toString();
-                    var filePath = path.dirname(file.history[0]);
-                    log('filePath =>' + filePath)
-                    var extname = path.extname(file.history[0]);
-                    fileContent = fileContent.replace(REG_HTML, function(matchString){
-                        MATCHED++;
-                        var url = matchString.replace(REG_SUB, '');
-                        log('\n' + matchString + ' => ' + url);
-                        var absoluteUrl;
-                        if(url.indexOf('/') === 0){
-                            absoluteUrl = path.join(base, url);
-                        }else{
-                            absoluteUrl = path.resolve(base, filePath, url);
-                        }
-                        log('absoluteUrl => ' + absoluteUrl);
-                        var relativeUrl = path.relative(filePath, absoluteUrl);
-                        log('ralvativeUrl => ' + relativeUrl);
-                        var absoluteUrlInHtml = absoluteUrl.replace(base, '');
-                        log('absoluteUrlInHtml => ' + absoluteUrlInHtml);
-                        var result;
-                        if(options.mode == 'relative'){
-                            result = matchString.split(url).join(relativeUrl);
-                        }else{
-                            result = matchString.split(url).join(absoluteUrlInHtml);
-                        }
-                        console.log('result: ' + matchString + ' => ' + result);
-                        REPLACED++;
-                        return result;
-                    });
-                    file.contents = new Buffer(fileContent);
-                    return callback(null, file);
-                }
-
-                callback(null, file);
-            }
-
-            if (options && options.skipBinary) {
-                istextorbinary.isText(file.path, file.contents, function(err, result) {
-                    if (err) {
-                        return callback(err, file);
-                    }
-
-                    if (!result) {
-                        callback(null, file);
-                    } else {
-                        doReplace();
-                    }
-                });
-
-                return;
-            }
-
-            doReplace();
-
-            console.log('\nFOUND:' + MATCHED + ' REPLACED:' + REPLACED + '\n');
+      function doReplace() {
+        if (file.isStream()) {
+          file.contents = file.contents.pipe(rs(search, replacement));
+          return callback(null, file);
         }
-    });
+
+        if (file.isBuffer()) {
+          console.log('\nchecking file...' + file.history[0]);
+          let fileContent = file.contents.toString();
+          const filePath = path.dirname(file.history[0]);
+          log('filePath =>' + filePath);
+          //   const extname = path.extname(file.history[0]);
+          fileContent = fileContent.replace(REG_HTML, function(matchString) {
+            MATCHED++;
+            const url = matchString.replace(REG_SUB, '');
+            log('\n' + matchString + ' => ' + url);
+            let absoluteUrl;
+            if (url.indexOf('/') === 0) {
+              absoluteUrl = path.join(base, url);
+            } else {
+              absoluteUrl = path.resolve(base, filePath, url);
+            }
+            log('absoluteUrl => ' + absoluteUrl);
+            const relativeUrl = path
+              .relative(filePath, absoluteUrl)
+              .split(path.sep)
+              .join('/');
+            log('ralvativeUrl => ' + relativeUrl);
+            const absoluteUrlInHtml = absoluteUrl
+              .replace(base, '')
+              .split(path.sep)
+              .join('/');
+
+            log('absoluteUrlInHtml => ' + absoluteUrlInHtml);
+            let result;
+            if (options.mode == 'relative') {
+              result = matchString.split(url).join(prefix + relativeUrl);
+            } else {
+              result = matchString.split(url).join(prefix + absoluteUrlInHtml);
+            }
+            log('result: ' + matchString + ' => ' + result);
+            REPLACED++;
+            return result;
+          });
+          file.contents = new Buffer(fileContent);
+          return callback(null, file);
+        }
+
+        callback(null, file);
+      }
+
+      if (options && options.skipBinary) {
+        istextorbinary.isText(file.path, file.contents, function(err, result) {
+          if (err) {
+            return callback(err, file);
+          }
+
+          if (!result) {
+            callback(null, file);
+          } else {
+            doReplace();
+          }
+        });
+
+        return;
+      }
+
+      doReplace();
+
+      console.log('\nFOUND:' + MATCHED + ' REPLACED:' + REPLACED + '\n');
+    },
+  });
 };
